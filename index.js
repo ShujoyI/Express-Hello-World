f4a66e5a5db0b98e9785d02daaa0c8e4b3832a3d
ShujoyI <shujoyi@gmail.com>
2020-04-03 14:32:43 -0700

    Update index.js
    Created sample express app

--- index.js
+++ index.js
@@ -3,5 +3,9 @@ var express = requre('express');
 var app = express();
 
 app.get('/', function(req, res) {
-    
-})
+    res.send('Hello World!');
+});
+
+app.listen(3000, function() {
+    console.log('Example app listening on port 3000!');
+});

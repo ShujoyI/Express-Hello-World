fb5e6063b4e072a01c432d00f4a0cfd907342fc7
ShujoyI <shujoyi@gmail.com>
2020-04-04 18:28:13 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
-//import the Express module
+// import the Express module
 var express = require('express');
 var app = express();
 
-//import the square module
+// create router object
+var router = express.Router();
+
+// create a route to /wow
+var wowzer = router.get('/wow', function(req, res) {
+    res.send('Wow the router works');
+});
+
+// going to /wow uses the router object
+app.use('/', wowzer);
+
+// import the square module
 var square = require('./square');
 console.log('The area of a square of width 3 is ' + square.area(4));
+
 
 app.get('/', function(req, res) {
     res.send('Hello World!!');
 });
+
 
 app.listen(3000, function() {
     console.log('Example app listening on port 3000!');
